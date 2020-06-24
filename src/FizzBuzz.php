e76f82cd71594ddbdf6b40c79a91ac4132f60f40
michal.lipski <kiwaczki@gmail.com>
2020-06-24 15:50:46 +0200

    added starting class and sample test

---
+++ src/FizzBuzz.php
@@ -0,0 +1,9 @@
+<?php
+
+class FizzBuzz
+{
+    public function calculate(): string
+    {
+        return "";
+    }
+}
--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -1,13 +1,19 @@
 <?php
 
 namespace tests;
+use FizzBuzz;
 use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
 
-    public function test___START_HERE() {
-        $this->assertEquals(5, 5);
+    public function test___FizzBuzz_example() {
+//        This is just a sample test. Start here with your tests to test drive FizzBuzz.
+        $fizzBuzz = new FizzBuzz();
+
+        $result = $fizzBuzz->calculate();
+
+        $this->assertEquals($result, null);
     }
 
 }
